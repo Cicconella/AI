a2381b117b0c89c1a4a01e3d6ddd49c702801fb3
ludygrv17 <ludy17ludy@gmail.com>
2017-03-16 17:46:52 -0300

    Pacote de identificar Maior componente

--- liver.R
+++ liver.R
@@ -1,13 +1,18 @@
 #### Libraries and functions ####
 
 #install.packages("oro.dicom")
+#biocLite("EBImage")
+
+source("https://bioconductor.org/biocLite.R")
 library(oro.dicom) # Leitura do arquivo DICOM
 library(imager) # Blur da imagem
 library(mmand) # Operacoes morfologicas (abertura e fechamento) 
 library(e1071) #Clusterizacao FCM
 library(wavelets) # Realizar transformacao DWT
 library(radiomics) # Gerar a matrix GLCM e extrair features
+library(EBImage) # Identificar Maior Componente de Imagem Binaria
 
+# Plotar Imagens
 plot_image <- function(m,nome){
   image(t(m), col=grey(0:64/64), axes=FALSE, xlab=nome, ylab="")
 }
@@ -18,6 +23,7 @@ plot_matrix <- function(m, nome){
   plot_image(m[nrow(m):1,],nome)  
 }
 
+# Transformacao Wavelet
 dwt_rows <- function(m){
   dim_m = dim(m)[1]
   wt=list()
@@ -39,6 +45,7 @@ dwt_matrix = function(m){
   return(m3)
 }
 
+# Preprocessamento
 normaliza = function(x, lo, hi){
   if(x<lo){
     return(0)
@@ -50,9 +57,26 @@ normaliza = function(x, lo, hi){
   }
 }
 
-Mode <- function(x) {
+# Preprocessamento
+moda <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
+}
+
+limpa_preto <- function(m){
+  m = m[-which((apply(m, 1, sum))==0),]
+  m = m[,-which((apply(m, 2, sum))==0)]
+  return(m)
+}
+
+# Limpeza
+maior_componente <- function(a) {
+  m = bwlabel(a)  
+  tam = max(table(m)[-1])
+  n = which.max(table(m)[-1])
+  m[m!=n]=0
+  m[m==n]=1
+  return(list(matrix=m, max=tam))
 }
 
 ##### Nossos dados ###### 
@@ -60,46 +84,37 @@ Mode <- function(x) {
 ### Para um arquivo ###
 
 #dir = "/media/cicconella/8AA6013CA6012A71/Users/Nina/Documents/Machiron/52490000/52490000/"
-dir = "/Users/ludykong/MaChiron/Data/HCC Lirads 5/"
-filename = "1.2.840.113619.2.327.3.1091195278.42.1381225064.881.121.dcm"
-#filename = 65643294
+
+#dir = "/Users/ludykong/MaChiron/Data/HCC Lirads 5/"
+#dir = "/Users/ludykong/MaChiron/Data/Hemangioma grande lobo esquerdo/"
+dir = "/Users/ludykong/MaChiron/Data/52490000/"
+
+#filename = "1.2.840.113619.2.327.3.1091195278.42.1381225064.881.121.dcm"
+#filename = "1.2.840.113704.1.111.5852.1422287786.17764.dcm"
+filename = 65643294
+
 fname <-  paste(dir, filename, sep="")
 
 abdo <- readDICOMFile(fname)
 #names(abdo)
 
-#abdo$hdr
-abdo$hdr[abdo$hdr$name == "StudyDescription",]
-abdo$hdr[abdo$hdr$name == "SeriesDescription",]
+# Dados Importantes do Header
 
-#dim(abdo$hdr)
-abdo$hdr[30,]
-abdo$hdr[12,]
-which(abdo$hdr[,3]=="SliceLocation")
-abdo$hdr[125,6]
+# abdo$hdr
+# which(abdo$hdr[,3]=="SliceLocation")
+# abdo$hdr[abdo$hdr$name == "StudyDescription",]
+# abdo$hdr[abdo$hdr$name == "SeriesDescription",] #30
+# abdo$hdr[abdo$hdr$name == "ImageType",] #10 ORIGINAL PRIMARY AXIAL
+# abdo$hdr[abdo$hdr$name == "SliceThickness",] #64
+# abdo$hdr[abdo$hdr$name == "SliceLocation",] #125
+# abdo$hdr[abdo$hdr$name == "PixelSpacing",] #158 0.703125 0.703125
 
 
 plot_image(abdo$img, "Original")
-dim(abdo$img)
 
 a = abdo$img
-dim(a)
-
-#min(apply(a, 1, min))
-#max(apply(a, 1, max))
-
 a = as.array(a)
-length(a)
-
 hist(a[a>100], nc=100000)
-
-sqrt(var(as.vector(a)))
-
-b = as.vector(a[a>750])
-b = b[b<1250]
-
-summary(b)
-hist(b, nc=100)
 
 ##### Normalizar #####
 
@@ -112,15 +127,8 @@ for(i in 1:dim(a)[1]){
   }
 }
 plot_image(normalizada, "Normalizada")
-#hist(a[a>0])
-
-min(a)
-max(a)
-
-dim(a)
 
 normalizada = a
-rm(a)
 
 ##### Recorte da janela com o figado #####  
 dim(normalizada)
@@ -133,8 +141,7 @@ max_y = floor(pikachu*0.25)
 janela = normalizada
 janela = janela[,-c(min_x:max_x)]
 janela = janela[-c(min_y:max_y),]
-janela = janela[,-which((apply(janela, 2, sum))==0)]
-janela = janela[-which((apply(janela, 1, sum))==0),]
+janela = limpa_preto(janela)
 
 plot_image(janela, "Janela")
 
@@ -150,18 +157,20 @@ plot_image(filtrada, "Filtrada")
 
 ##### Analise do histograma #####
 
-#hist(filtrada, nc=100)
-#hist(filtrada[-c(which(filtrada<50), which(filtrada>225))], nc = 1000)
+hist(filtrada, nc=100)
+hist(filtrada[-c(which(filtrada<100), which(filtrada>225))], nc = 1000)
 filtrada = round(filtrada)
-moda = Mode(filtrada[-c(which(filtrada<50), which(filtrada>225))])
+mod = moda(filtrada[-c(which(filtrada<100), which(filtrada>225))])
+mod
 dp = 25
 
-##### Aplicacoes morofologicas #####
+##### Aplicacoes morfologicas #####
+
 
 binaria = filtrada
 
-binaria[binaria>(moda+dp)] = 0
-binaria[binaria<(moda-dp)] = 0
+binaria[binaria>(mod+dp)] = 0
+binaria[binaria<(mod-dp)] = 0
 binaria[binaria!=0] = 1
 
 
@@ -172,85 +181,56 @@ kernel <- shapeKernel(c(13,13), type="disc")
 binaria = opening(binaria, kernel)
 binaria = closing(binaria, kernel)
 
+#tamfig = table(binaria)[ ]
 plot_image(binaria, "Binaria Pos Morfo")
 
-morfo = binaria * filtrada
+dim(binaria)
+l = maior_componente(binaria)
+maior_binaria = l$matrix
+tamanho_figado = l$max
+plot_image(maior_binaria, "Maior componente da Binaria")
 
-#morfo = morfo[-which(apply(morfo, 1, sum)==0),]
-#morfo = morfo[,-which(apply(morfo, 2, sum)==0)]
+morfo = maior_binaria * filtrada
+morfo = limpa_preto(morfo)
 plot_image(morfo, "Morfo")
-#hist(morfo[morfo>150], nc = 1000)
 
 ##### FCM #####
-
-linha = as.vector(morfo)
-length(linha)
-
-#hist(morfo[morfo>100], nc=100)
-tmp=proc.time()
-resultado = cmeans(linha, iter.max = 150, centers=c(0, moda/2, moda), dist = "manhattan", m = 1.5)
-proc.time()-tmp
-#(resultado$centers)
-
-#dim(resultado$membership)
-
-#vetor = c(0, 0.85, 0.15)
-
-# clusteriza = function(vetor){
+# linha = as.vector(morfo)
+# #hist(morfo[morfo>100], nc=100)
 # 
-#   if(vetor[1]>0.9){
-#     return(1)
-#   }else if(vetor[2]>0.5){
-#     return(2)
-#   } else {
-#     return(3)
-#   }
-#     
-# }
-
-#cluster = apply(resultado$membership, 1, clusteriza)
-
-#length(cluster)
-
-#hist(resultado$cluster)
-
-#cluster = matrix(cluster, nrow = 233)
-
-cluster = matrix(resultado$cluster, nrow = dim(morfo)[1])
-
-plot_image(cluster,"Clusters")
-
-kernel = shapeKernel(c(5,5), type="disc")
-
-limpo = cluster
-
-limpo = closing(limpo, kernel)
-limpo = opening(limpo, kernel)
-
-plot_image(limpo, "Limpo Clusters")
-
-limpo[limpo == 1] = 0
-limpo[limpo != 0] = 1
+# resultado = cmeans(linha, iter.max = 150, centers=c(0, mod/2, mod), dist = "manhattan", m = 1.5)
+# #(resultado$centers)
+# #dim(resultado$membership)
+# 
+# cluster = matrix(resultado$cluster, nrow = dim(morfo)[1])
+# plot_image(cluster,"Clusters")
+# 
+# kernel = shapeKernel(c(5,5), type="disc")
+# 
+# limpo = cluster
+# limpo = closing(limpo, kernel)
+# limpo = opening(limpo, kernel)
+# 
+# plot_image(limpo, "Limpo Clusters")
+# 
+# # Selecionar Cluster 2 e 3
+# limpo_2 = limpo
+# limpo_2[limpo_2 == 1] = 0
+# limpo_2[limpo_2 != 0] = 1
 
 ##### Extracao de caracteristicas de textura #####
-
-detalhes = limpo*morfo
+detalhes = morfo
 
 plot_matrix(detalhes, "Lesao")
 
+# Recorta Janela quadrada de lado par
 M = min(dim(detalhes)) 
 if (M%%2 == 1) M = M-1
 detalhes = detalhes[1:M, 1:M]
-
 plot_matrix(detalhes, "Janela Detalhes")
+
 dwt_detalhes=dwt_matrix(detalhes)
 plot_matrix(dwt_detalhes, "Detalhes")
-
-#class(detalhes)
-#dim(detalhes)
-
-min(detalhes)
-max(detalhes)
 
 # Extracao de features de textura com a matrix de dependencia de niveis de cinza (GLCM)
 m = glcm(detalhes, angle=0,d=1)
